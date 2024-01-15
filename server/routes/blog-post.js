728fb2b231ada8c4fed4718036bc8b2f47988cfb
Ammar-64 <ammar.n.sammour@gmail.com>
2024-01-15 13:33:21 +0000

    Add Docker instructions to README.md and fix test command

--- server/routes/blog-post.js
+++ server/routes/blog-post.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const blogPostController = require("../../controllers/blog-post")
+const blogPostController = require("../controllers/blog-post");
 
 router.get("/", blogPostController.getAllBlogPosts);
 router.post("/", blogPostController.addBlogPost);
-router.get("/filter", blogPostController.filterBlogPosts)
-router.get("/:id", blogPostController.getOneBlogPost);;
+router.get("/filter", blogPostController.filterBlogPosts);
+router.get("/:id", blogPostController.getOneBlogPost);
 router.put("/:id", blogPostController.updateBlogPost);
 router.delete("/:id", blogPostController.removeBlogPost);
 router.post("/:id/likes", blogPostController.updateLikes);
