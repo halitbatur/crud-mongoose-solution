b34e77c37b428e4092c56978893e7aaf80926d42
Ammar-64 <ammar.n.sammour@gmail.com>
2024-01-15 17:40:13 +0000

    Fix test and DB connection

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 
-const url = process.env.DB_URL;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
+  process.env;
+
+const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
+  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
+}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
+const url = DB_URI;
 
 const connectToMongo = () => {
   mongoose.connect(url, { useNewUrlParser: true });
@@ -14,6 +21,6 @@ const connectToMongo = () => {
   db.on("error", (err) => {
     console.error("Database connection error: ", err);
   });
-}
+};
 
 module.exports = connectToMongo;
